9acdc69190b8681410ae13fd8650b5ef45ff7d9a
ohminsun <ohminsun@gmail.com>
2015-06-07 15:55:45 +0000

    added header

--- add_bootstrap.php
+++ add_bootstrap.php
@@ -4,7 +4,8 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
     </head>
     <body>
-        <?php echo "Hi!  This is a test page referencing Bootstrap.  I'm Lynn.  I'm trying to learn PHP (the right way)."
-        ?>
+        <h3>Welcome to the PHP Jungle.</h3>
+        <p><?php echo "Hi!  This is a test page referencing Bootstrap.  I'm Lynn.  I'm trying to learn PHP (the right way)."
+            ?></p>
     </body>
 </html>

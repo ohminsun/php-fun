066976b757b57516996c86d283c29fa31888c4a0
ohminsun <ohminsun@gmail.com>
2015-06-07 15:52:25 +0000

    created page referencing bootstrap for style

---
+++ add_bootstrap.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>A little test page with some Bootstrap</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+    </head>
+    <body>
+        <?php echo "Hi!  This is a test page referencing Bootstrap.  I'm Lynn.  I'm trying to learn PHP (the right way)."
+        ?>
+    </body>
+</html>

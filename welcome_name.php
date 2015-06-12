26e1a7bf466c2de4e954c6fd6e298fb1738ec5eb
ohminsun <ohminsun@gmail.com>
2015-06-12 12:09:29 -0500

    created welcome page with name

---
+++ welcome_name.php
@@ -0,0 +1,16 @@
+<?php
+/* get the data from the request */
+$first_name = $_GET['first_name'];
+$last_name = $_GET['last_name'];
+?>
+
+<html>
+<head>
+<title>Welcome</title>
+</head>
+<body>
+<h2>Welcome</h2>
+<p>First name: <?php echo $first_name; ?></p>
+<p>Last name: <?php echo $last_name; ?></p>
+</body>
+</html>

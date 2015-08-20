e7272e6b4a6eeaf1604a169c991cc84e227d4e81
ohminsun <ohminsun@gmail.com>
2015-08-20 13:53:30 -0500

    added new tutorial

---
+++ test/index.php
@@ -0,0 +1,11 @@
+<?php 
+	// $users= array('first name' => 'Brad', 'last name' => 'Traversy', 'email' => 'techguyinfo@gmail.com');
+	// $fruits = array('Apple', 'Orange', 'Grapes');
+
+	function sayIt($words) {
+		echo $words;
+	}
+
+	sayIt('Hello Again');
+
+?>
